fb43c45a954e5b7d76b6917442746487c206710b
christwy0614 <christwy0614@gmail.com>
2022-07-11 21:58:17 -0500

    add new file

---
+++ app/src/calculator.py
@@ -0,0 +1,11 @@
+class Calculator(): 
+    def add(self, num1, num2):
+       # return num1 + num2
+       return 2
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    print(calculator.add(1,3))
+    
+
+
